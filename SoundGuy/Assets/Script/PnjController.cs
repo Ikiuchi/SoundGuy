5c5b20b8dc1cbcd9bb8bef2c2c5ac569efcc28f9
Alexandre <ikiuchi@hotmail.fr>
2020-09-08 15:22:44 +0200

    add AI jump

---
+++ SoundGuy/Assets/Script/PnjController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PnjController : MonoBehaviour
+{
+    public Transform player = null;
+    public float moveSpeed = 10.0f;
+    public bool follow = true;
+    public NavMeshAgent navmesh;
+
+    public CapsuleCollider caps;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        navmesh = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+
+        caps = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        if (follow)
+            Movement();
+    }
+
+    public void Movement()
+	{
+        if (navmesh.enabled)
+            navmesh.destination = player.position;
+    }
+
+    public void Charmed()
+	{
+        follow = true;
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.layer == LayerMask.NameToLayer("Jump"))
+            Jump();
+        else if (other.gameObject.layer == LayerMask.NameToLayer("Dash"))
+            Dash();
+    }
+
+	public void OnCollisionEnter(Collision collision)
+	{
+        navmesh.enabled = true;
+        rb.isKinematic = true;
+        rb.useGravity = false;
+        caps.isTrigger = true;
+    }
+
+	private void Jump()
+	{
+        navmesh.enabled = false;
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        caps.isTrigger = false;
+
+        Vector3 dir = player.position - transform.position;
+
+        rb.AddForce(dir.x * 2, Vector3.up.y * 100, dir.z * 2);
+    }
+
+    private void Dash()
+	{
+        Debug.Log("Dash");
+    }
+}
