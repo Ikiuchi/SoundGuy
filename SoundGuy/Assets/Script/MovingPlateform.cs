7f5868607e5b194dc34a3e3e896dc09df04f8582
Lepec <n.lepec@student.isartdigital.com>
2020-09-10 11:17:51 +0200

    moving platform fixed + timer on it

--- SoundGuy/Assets/Script/MovingPlateform.cs
+++ SoundGuy/Assets/Script/MovingPlateform.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 public class MovingPlateform : MonoBehaviour
 {
     public float movingSpeed = 5;
-    public Vector2 translation = Vector2.zero;
+    public Transform translation;
+    public float timerStop = 1f;
+
+    private float currentTimer = 0;
 
     private Vector3 startingPoint;
     private Vector3 endingPoint;
+
+    Vector3 direction;
 
     bool goingToRight = true;
 
     private void Start()
     {
         startingPoint = transform.position;
-        endingPoint = transform.position + new Vector3(translation.x, translation.y , 0);
+        endingPoint = translation.position;
+
+        direction = (endingPoint - startingPoint).normalized;
     }
 
     private void Update()
     {
         isGoingToRight();
         Movement();
+        if (currentTimer > 0)
+            currentTimer -= Time.deltaTime;
     }
 
     private void isGoingToRight()
@@ -33,14 +42,48 @@ public class MovingPlateform : MonoBehaviour
             distance = Vector3.Distance(startingPoint, transform.position);
 
         if (distance <= 0.2)
+        {
+            currentTimer = timerStop;
             goingToRight = !goingToRight;
+        }
+       
     }
 
     private void Movement()
     {
-        if(goingToRight)
-            transform.Translate(transform.right * movingSpeed * Time.deltaTime);
-        else
-            transform.Translate(-transform.right * movingSpeed * Time.deltaTime);
+        if (currentTimer <= 0)
+        {
+            if (goingToRight)
+            {
+                transform.position = transform.position + (direction * movingSpeed * Time.deltaTime);
+                //GetComponent<Rigidbody>().MovePosition(transform.position + (transform.right * movingSpeed * Time.deltaTime));
+            }
+            else
+                transform.position = transform.position + (-direction * movingSpeed * Time.deltaTime);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Player>() != null)
+        {
+            collision.transform.parent = transform;
+        }
+        else if (collision.gameObject.GetComponent<PnjController>() != null)
+        {
+            collision.transform.parent = transform;
+        }
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Player>() != null)
+        {
+            collision.transform.parent = null;
+        }
+        else if (collision.gameObject.GetComponent<PnjController>() != null)
+        {
+            collision.transform.parent = null;
+        }
+
     }
 }
