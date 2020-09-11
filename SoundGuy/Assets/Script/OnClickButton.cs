f006b125679cba8a4cf046f46bf1756b627000c2
Lepec <n.lepec@student.isartdigital.com>
2020-09-11 10:15:26 +0200

    sound menuGame

--- SoundGuy/Assets/Script/OnClickButton.cs
+++ SoundGuy/Assets/Script/OnClickButton.cs
@@ -7,37 +7,6 @@ using UnityEngine.Audio;
 
 public class OnClickButton : MonoBehaviour
 {
-    public AudioClip[]  audioClips;
-    public Button[]     buttons;
-    public AudioMixer   mixer;
-
-    private AudioSource[] source;
-    public GameObject SoundClick;
-    // Start is called before the first frame update
-    void Start()
-    {
-        source = new AudioSource[audioClips.Length];
-        if (!mixer)
-            return;
-
-        for (int i = 0; i < source.Length; i++)
-        {
-            source[i] = SoundClick.AddComponent<AudioSource>();
-            source[i].clip = audioClips[i];
-            source[i].loop = true;
-            source[i].playOnAwake = false;
-            source[i].outputAudioMixerGroup = mixer.FindMatchingGroups("Master")[2];
-            //source[i].Play();
-        }
-
-        /*for (int i = 0; i < buttons.Length; i++)
-        {
-            Debug.Log("i " + i);
-            buttons[i].onClick.AddListener(() => PlaySound(source[i]));
-            source[i].Play();
-        }*/
-    }
-
     public void PlaySound(AudioSource source)
     {
         Debug.Log("hi");
